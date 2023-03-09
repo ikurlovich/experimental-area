eaaadad6b36c34388544ee16b8717f020cfa50f8
ikurlovich <kia555587937@gmail.com>
2023-03-10 03:26:01 +0400

    Initial Commit

---
+++ experimental area/experimental_areaApp.swift
@@ -0,0 +1,17 @@
+//
+//  experimental_areaApp.swift
+//  experimental area
+//
+//  Created by Илья Курлович on 10.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct experimental_areaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
